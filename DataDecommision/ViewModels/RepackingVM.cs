e7d9d2f28684344e684007009adb16659dac7b7d
Rahul Chandhuvarath <luharulal@gmail.com>
2023-04-28 18:14:25 +0530

    Add Repacking Page

---
+++ DataDecommision/ViewModels/RepackingVM.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls.Primitives;
+using System.Windows.Input;
+
+namespace DataDecommision
+{
+    internal class RepackingVM : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        /// <summary>
+        /// Method to call when the property value is changed
+        /// </summary>
+        /// <param name="propertyName"></param>
+        protected void NotifyPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+        public RepackingVM()
+        {
+            SelectedDate = null;
+            ButtonScanClick = new ButtonCommandBinding(ButtonScan)
+            {
+                IsEnabled = true
+            };
+            ButtonNextClick = new ButtonCommandBinding(ButtonNext)
+            {
+                IsEnabled = true
+            };
+
+            ButtonBackClick = new ButtonCommandBinding(ButtonBack)
+            {
+                IsEnabled = true
+            };
+        }
+
+
+
+        private DateTime? _selectedDate;
+        public DateTime? SelectedDate
+        {
+            get { return _selectedDate; }
+            set
+            {
+                if (_selectedDate != value)
+                {
+                    _selectedDate = value;
+                    NotifyPropertyChanged(nameof(SelectedDate));
+                }
+            }
+        }
+        public ButtonCommandBinding ButtonBackClick { get; set; }
+        public ButtonCommandBinding ButtonScanClick { get; set; }
+        public ButtonCommandBinding ButtonNextClick { get; set; }
+
+      
+        private string textGtin;
+        public string TextGtin
+        {
+            get { return textGtin; }
+            set { textGtin = value; NotifyPropertyChanged("TextGtin"); }
+        }
+
+        private string textLot;
+        public string TextLot
+        {
+            get { return textLot; }
+            set { textLot = value; NotifyPropertyChanged("TextLot"); }
+        }
+
+        private string textEXP;
+        public string TextEXP
+        {
+            get { return textEXP; }
+            set { textEXP = value; NotifyPropertyChanged("TextEXP"); }
+        }
+
+        private bool scaningPopup;
+        public bool ScaningPopup
+        {
+            get { return scaningPopup; }
+            set { scaningPopup = value; NotifyPropertyChanged("ScaningPopup"); }
+        }
+        private async void ButtonScan()
+        {
+            Mouse.OverrideCursor = Cursors.Wait;
+            ScaningPopup = true;
+
+            await Task.Run(() =>
+            {
+
+                var code = ScannerDecoder.ScanAndDecode();
+                TextGtin = code.Item1;
+                TextLot = code.Item2;
+                if (code.Item3 != null && code.Item3 != "")
+                {
+                    DateTime expResult = DateTime.ParseExact(20 + code.Item3, "yyyyMMdd", System.Globalization.CultureInfo.InvariantCulture);
+                    SelectedDate = expResult;
+                }
+                else
+                {
+                    SelectedDate = null;
+                }
+
+            });
+            Mouse.OverrideCursor = null;
+            ScaningPopup = false;
+        }
+        public void ButtonBack()
+        {
+
+            NavigationService navigationService = (NavigationService)App.Current.MainWindow.Resources["NavigationService"];
+            navigationService.CurrentPage = BulkItemPage.Instance;
+        }
+        public void ButtonNext()
+        {
+
+            //NavigationService navigationService = (NavigationService)App.Current.MainWindow.Resources["NavigationService"];
+            //navigationService.CurrentPage = new RePackingPage();
+        }
+
+
+    }
+
+}
---
+++ DataDecommision/Views/RePackingPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DataDecommision
+{
+    /// <summary>
+    /// Interaction logic for RePackingPage.xaml
+    /// </summary>
+    public partial class RePackingPage : Page
+    {
+        public RePackingPage()
+        {
+            InitializeComponent();
+            this.DataContext = new RepackingVM();
+        }
+    }
+}
